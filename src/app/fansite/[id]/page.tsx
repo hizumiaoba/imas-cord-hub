70e9d81cc1ae0d1e9e15cf47132d411609e85d45
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-07-19 16:43:01 +0900

    set front page

--- src/app/fansite/[id]/page.tsx
+++ src/app/fansite/[id]/page.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
-import Individuals from "../components/Individuals";
+import Individuals from "../../components/Individuals";
 
 const Page = ({ params }: { params: { id: string } }) => {
   return (
     <Suspense fallback={<h2>Now Loading...</h2>}>
-      <Individuals id={parseInt(params.id)} />
+      <Individuals id={Number(params.id)} />
     </Suspense>
   );
 };
