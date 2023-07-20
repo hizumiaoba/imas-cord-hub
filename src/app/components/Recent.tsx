15237b2a039db9aeb9a95986bf7e03bbc58852b8
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-07-20 22:15:07 +0900

    add another comps

--- src/app/components/Recent.tsx
+++ src/app/components/Recent.tsx
@@ -5,6 +5,20 @@ export const Recent = async () => {
   return (
     <div className={`${pageStyle.section} ${pageStyle.sectionTitle} recently-added`}>
       <h2>Recently Added</h2>
+      <p style={{color: "rgb(255,255,255,0.7", fontSize: "0.8rem"}}>Shows Recently added to this site.</p>
+
+      <div className={pageStyle.grid}>
+
+        <a href="#" className={pageStyle.card}>
+          <h3>Card 1</h3>
+          <p>Card 1</p>
+        </a>
+
+        <a href="#" className={pageStyle.card}>
+          <h3>Card 2</h3>
+          <p>Card 2</p>
+        </a>
+      </div>
     </div>
   )
 };
