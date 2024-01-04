30317ff19d7f9c5c7a1bcde284b43f4159946ce9
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2024-01-04 10:53:00 +0900

    Update base API URL and add getRandomPickedArray function

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,4 +1,4 @@
-export const baseApiUrl = 'https://imas-cord-hub-api.vercel.app/api';
+export const baseApiUrl = 'https://imas-cord-hub-backend-api.vercel.app/api';
 
 export const selectIpColor = (ip: string): string => {
   switch (ip) {
@@ -18,3 +18,14 @@ export const selectIpColor = (ip: string): string => {
       return '#ff74b8'; // imas brand color
   }
 }
+
+export const getRandomPickedArray = <T extends any>(arr: Array<T>, count: number): Array<T> => {
+  let shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
+  while (i-- > min) {
+    index = Math.floor((i + 1) * Math.random());
+    temp = shuffled[index];
+    shuffled[index] = shuffled[i];
+    shuffled[i] = temp;
+  }
+  return shuffled.slice(min);
+};
